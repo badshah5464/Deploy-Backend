4c39fc76e86ce56b17cc37213924ffe08cd63b45
Dev Dave <115370760+badshah5464@users.noreply.github.com>
2023-05-26 18:23:25 +0530

    .

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../Img/Dw-Logo.svg";
+
+const Header = () => {
+  return (
+    <div>
+      <img src={Logo} alt="Logo" className="w-10" />
+    </div>
+  );
+};
+
+export default Header;
